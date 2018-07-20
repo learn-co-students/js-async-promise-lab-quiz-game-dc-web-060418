54177ba98a5975ccfc238f0147cecdb73e358478
github username <github email address>
2018-07-20 16:17:40 -0400

    finished

--- index.js
+++ index.js
@@ -4,17 +4,26 @@ const questions = [
   {questionText: "Goldfish only have a memory of about three seconds", answer: "false"}
 ]
 
-let question = questions[0]
+let index = 0
+let question = questions[index]
+
 
 document.addEventListener("DOMContentLoaded", function(){
   document.querySelector("#ask").addEventListener("click", function(){
-    question = nextQuestion()
+    nextQuestion()
     displayQuestionOnClick()
   })
 });
 
-function nextQuestion(){
 
+function nextQuestion(){
+  if(index === questions.length - 1) {
+    index = 0
+    question = questions[index];
+  } else {
+    index++
+    question = questions[index]
+  }
 }
 
 function displayQuestionOnClick(){
@@ -22,21 +31,49 @@ function displayQuestionOnClick(){
 }
 
 function askQuestionThen(time){
-
+  appendQuestion()
+  return new Promise((resolve, reject) => {
+    document.querySelector(".answer-buttons").addEventListener("click", function(event){
+      if(question.answer === event.target.id) {
+        console.log("answer is correct")
+        resolve(true)
+      } else {
+        console.log("answer is incorrect")
+        resolve(false);
+      }
+    })
+    setTimeout(function(){
+      resolve(false)
+    }, time)
+  })
 }
 
 function askQuestionThenRemoveQuestion(time){
-
+  askQuestionThen(time).then(resolve => {
+    let score = document.getElementById("score")
+    if (resolve=== true) {
+      score.innerText = parseInt(score.innerText) + 1
+    } else if (resolve === false) {
+      score.innerText = 0;
+    }
+      removeQuestion(question)
+  })
 }
 
 function appendQuestion(){
-
+  container = document.querySelector(".question-container")
+  container.innerText = question.questionText;
+  toggleButtons()
 }
 
 function removeQuestion(){
-
+  container = document.querySelector(".question-container")
+  container.innerText = ""
+  toggleButtons()
 }
 
 function toggleButtons(){
-
+  document.querySelector("#ask").classList.toggle("hide")
+  document.querySelector("#true").classList.toggle("hide")
+  document.querySelector("#false").classList.toggle("hide")
 }
